7036b8d6ca2ea0fb95d6a24f1185234648cf3bcc
nastya <hociy2015@gmail.com>
2026-03-03 17:38:34 +0200

    Implement books model and basic api structure

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import  viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [AllowAny()]
+        return [IsAdminUser()]
