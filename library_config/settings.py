e2489d249f6c8177966a02fe179a1baf34302978
nastya <hociy2015@gmail.com>
2026-03-11 12:49:31 +0200

    Hide secret key and cleanup admin modules

--- library_config/settings.py
+++ library_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w5v@f)bn5c6zq#&*mos5ih)!^_utx*o+!6$2@)jb7=i=r%6cdc"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-if-not-found")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,7 +151,6 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-import os
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
